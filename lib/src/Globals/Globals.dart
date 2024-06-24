744e5a4788a025f356bb5826e1ae6228c4558094
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-24 14:42:16 -0700

    Globals Updates

--- lib/src/Globals/Globals.dart
+++ lib/src/Globals/Globals.dart
@@ -1,3 +1,39 @@
+// Card Parameters
+double singleCardMaxHeight = 0.25;
+double singleCardMinHeight = 0.1;
+double singleCardMaxWidth = 0.35;
+double singleCardMinWidth = 0.25;
+double singleCardPadding = 16.0;
+double singleCardWidthBox = 0.05;
+
+// Dashboard Card Title Parameters
+String resumesGenTitle = 'Resumes Generated';
+String coverLettersGenTitle = 'Cover Letters Generated';
+
 // Documents generated
 int resumesGenerated = 0;
 int coverLettersGenerated = 0;
+
+// Drawer Parameters
+double drawerVerticalPadding = 25.0;
+double drawerWidth = 0.15;
+
+// Size Box Parameters
+double standardSizedBoxHeight = 20;
+double standardSizedBoxWidth = 20;
+
+// Text Parameters
+double appBarTitle = 24.0;
+double secondaryTitles = 16.0;
+
+// Tile Parameters
+double settingsTileContainerWidth = 0.6;
+
+// Title Parameters
+String dashboardTitle = 'Dashboard';
+
+// Tooltip Parameters
+String dashboardToolTip = 'Dashboard';
+String profileToolTip = 'Profile';
+String jobsToolTip = 'Job Description';
+String settingsToolTip = 'Settings';
