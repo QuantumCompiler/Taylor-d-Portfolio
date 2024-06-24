f5f81cfb3b74a9bd8304d25f2e50a37b96fac039
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-24 14:54:33 -0700

    Globals Updates

--- lib/src/Globals/Globals.dart
+++ lib/src/Globals/Globals.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
 // Card Parameters
 double singleCardMaxHeight = 0.25;
 double singleCardMinHeight = 0.1;
@@ -5,6 +8,12 @@ double singleCardMaxWidth = 0.35;
 double singleCardMinWidth = 0.25;
 double singleCardPadding = 16.0;
 double singleCardWidthBox = 0.05;
+
+// Colors
+Color cyanButtonColor = Color.fromARGB(255, 0, 213, 255);
+Color whiteButtonColor = Colors.white;
+Color blackTextColor = Colors.black;
+Color whiteTextColor = Colors.white;
 
 // Dashboard Card Title Parameters
 String resumesGenTitle = 'Resumes Generated';
@@ -17,6 +26,10 @@ int coverLettersGenerated = 0;
 // Drawer Parameters
 double drawerVerticalPadding = 25.0;
 double drawerWidth = 0.15;
+
+// Settings Parameters
+String settingsCurrentTheme = 'Switch Theme';
+String settingsDeleteAllProfiles = 'Delete All Profiles';
 
 // Size Box Parameters
 double standardSizedBoxHeight = 20;
@@ -31,6 +44,7 @@ double settingsTileContainerWidth = 0.6;
 
 // Title Parameters
 String dashboardTitle = 'Dashboard';
+String settingsTitle = 'Settings';
 
 // Tooltip Parameters
 String dashboardToolTip = 'Dashboard';
