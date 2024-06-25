c2beb7ea80c0608e55b10ff6b50d1beb69a3c139
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-24 23:45:32 -0700

    Load Profile Updates - Gen Updates

--- lib/src/Profile/LoadProfile.dart
+++ lib/src/Profile/LoadProfile.dart
@@ -4,8 +4,14 @@ import 'dart:io';
 import 'EditProfile.dart';
 import 'ProfileUtils.dart';
 
-class LoadProfilePage extends StatelessWidget {
+class LoadProfilePage extends StatefulWidget {
   const LoadProfilePage({super.key});
+
+  @override
+  LoadProfilePageState createState() => LoadProfilePageState();
+}
+
+class LoadProfilePageState extends State<LoadProfilePage> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Directory>>(
@@ -19,8 +25,19 @@ class LoadProfilePage extends StatelessWidget {
                 icon: const Icon(Icons.arrow_back),
                 onPressed: () {
                   Navigator.of(context).pop();
+                  setState(() {});
                 },
               ),
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.dashboard),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    Navigator.of(context).pop();
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
               title: Text(
                 profiles.isEmpty ? 'No Profiles' : 'Load Profiles',
                 style: TextStyle(
@@ -42,11 +59,73 @@ class LoadProfilePage extends StatelessWidget {
                             child: ListView.builder(
                               itemCount: profiles.length,
                               itemBuilder: (context, index) {
-                                return ListTile(
-                                  title: Text(profiles[index].path.split('/').last),
-                                  onTap: () {
-                                    Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfilePage(profileName: profiles[index].path.split('/').last)));
-                                  },
+                                return MouseRegion(
+                                  cursor: SystemMouseCursors.click,
+                                  child: ListTile(
+                                    title: Text(profiles[index].path.split('/').last),
+                                    trailing: IconButton(
+                                      icon: Icon(Icons.delete),
+                                      onPressed: () {
+                                        showDialog(
+                                          context: context,
+                                          builder: (BuildContext context) {
+                                            return AlertDialog(
+                                              title: Text(
+                                                "Delete ${profiles[index].path.split('/').last}?",
+                                                style: TextStyle(
+                                                  fontSize: appBarTitle,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                                textAlign: TextAlign.center,
+                                              ),
+                                              content: Text(
+                                                "Are you sure you want to delete this profile?",
+                                                style: TextStyle(
+                                                  fontSize: secondaryTitles,
+                                                ),
+                                                textAlign: TextAlign.center,
+                                              ),
+                                              actions: <Widget>[
+                                                Row(
+                                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                                  mainAxisAlignment: MainAxisAlignment.center,
+                                                  children: [
+                                                    ElevatedButton(
+                                                      child: Text("Cancel"),
+                                                      onPressed: () {
+                                                        Navigator.of(context).pop();
+                                                      },
+                                                    ),
+                                                    SizedBox(width: standardSizedBoxWidth),
+                                                    ElevatedButton(
+                                                      child: Text("Delete"),
+                                                      onPressed: () {
+                                                        DeleteProfile(profiles[index].path.split('/').last);
+                                                        Navigator.of(context).pop();
+                                                        setState(() {});
+                                                      },
+                                                    ),
+                                                  ],
+                                                ),
+                                              ],
+                                            );
+                                          },
+                                        );
+                                      },
+                                    ),
+                                    onTap: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => EditProfilePage(profileName: profiles[index].path.split('/').last),
+                                        ),
+                                      ).then(
+                                        (_) {
+                                          setState(() {});
+                                        },
+                                      );
+                                    },
+                                  ),
                                 );
                               },
                             ),
