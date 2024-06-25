1dd07aef7a871d41101bdc107bdfb3220ba51979
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-24 18:55:24 -0700

    Tests Update - Cleaned Test File

--- lib/src/tests/test.dart
+++ lib/src/tests/test.dart
@@ -1,12 +1,1 @@
 import '../Profile/ProfileUtils.dart';
-
-Profile myProfile = Profile(name: 'FlurdBop');
-
-void main() {
-  print(myProfile.name);
-  print(myProfile.education);
-  myProfile.name = 'Flurd Bopping';
-  myProfile.education = 'Education';
-  print(myProfile.name);
-  print(myProfile.education);
-}
