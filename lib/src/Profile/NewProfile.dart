9f02550637020d52de0a681d998af61cfd4935e1
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-24 18:57:07 -0700

    New Profile Commit

---
+++ lib/src/Profile/NewProfile.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import '../Globals/Globals.dart';
+import 'ProfileUtils.dart';
+import '../Themes/Themes.dart';
+
+class NewProfilePage extends StatelessWidget {
+  const NewProfilePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    Profile newProfile = Profile(init: true);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.dashboard),
+            onPressed: () {
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+        title: Text(
+          profileCreateNew,
+          style: TextStyle(
+            color: themeTextColor(context),
+            fontSize: appBarTitle,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            // Education
+            ...ProfileEntry(context, newProfile.educationTitle, newProfile.eduCont, profileEduHint),
+            // Experience
+            ...ProfileEntry(context, newProfile.experienceTitle, newProfile.expCont, profileExpHint),
+            // Extracurricular
+            ...ProfileEntry(context, newProfile.extracurricularTitle, newProfile.extCont, profileExpHint),
+            // Honors
+            ...ProfileEntry(context, newProfile.honorsTitle, newProfile.honCont, profileHonHint),
+            // Projects
+            ...ProfileEntry(context, newProfile.projectsTitle, newProfile.projCont, profileProjHint),
+            // References
+            ...ProfileEntry(context, newProfile.referencesTitle, newProfile.refCont, profileRefHint),
+            // Skills
+            ...ProfileEntry(context, newProfile.skillsTitle, newProfile.skillsCont, profileSkillsHint),
+          ],
+        ),
+      ),
+      // Bottom Navigation Bar
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.transparent,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Save Profile
+            ElevatedButton(
+              onPressed: () async {
+                // Show Dialog
+                await showDialog(
+                  context: context,
+                  builder: (context) {
+                    // Alert Dialog
+                    return AlertDialog(
+                      title: Text(
+                        'Enter Name Of Current Profile',
+                        style: TextStyle(
+                          fontSize: appBarTitle,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      content: TextField(
+                        controller: newProfile.nameCont,
+                        decoration: InputDecoration(hintText: "Profile Name"),
+                      ),
+                      actions: <Widget>[
+                        // Row
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            ElevatedButton(
+                              child: Text('Cancel'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            SizedBox(width: 20),
+                            ElevatedButton(
+                              child: Text('Save'),
+                              onPressed: () async {
+                                // newProfile.setProfName(newProfile.nameCont.text);
+                                // CreateNewProfile(context, controllers);
+                              },
+                            ),
+                          ],
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              child: Text('Save Profile'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
