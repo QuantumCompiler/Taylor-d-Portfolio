5ac9ff68e595df72936401d461150c59e52fa82f
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-24 23:45:07 -0700

    Edit Profile Updates - Gen Updates

--- lib/src/Profile/EditProfile.dart
+++ lib/src/Profile/EditProfile.dart
@@ -62,127 +62,33 @@ class EditProfilePage extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                prevProfile.setOverwriteFiles();
+                Navigator.of(context).pop();
+              },
+              child: Text('Overwrite'),
+            ),
+            SizedBox(width: standardSizedBoxWidth),
+            ElevatedButton(
+              onPressed: () => {},
+              child: Text('Set As Primary'),
+            ),
+            SizedBox(width: standardSizedBoxWidth),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Cancel'),
+            ),
+          ],
+        ),
+      ),
     );
-    // Future Builder
-    // return FutureBuilder<Profile>(
-    //   future: prevProfile.getProf(profileName),
-    //   builder: (context, snapshot) {
-    //     // If Connection State is Waiting
-    //     if (snapshot.connectionState == ConnectionState.waiting) {
-    //       return Center(child: CircularProgressIndicator());
-    //     }
-    //     // Else If Error
-    //     else if (snapshot.hasError) {
-    //       return Center(child: Text('Error: ${snapshot.error}'));
-    //     }
-    //     // Else If No Data
-    //     else if (!snapshot.hasData) {
-    //       return Center(child: Text('No data available'));
-    //     }
-    //     // Else
-    //     else {
-    //       final controllers = snapshot.data!;
-    //       // Scaffold
-    //       return Scaffold(
-    //         appBar: AppBar(
-    //           leading: IconButton(
-    //             icon: Icon(Icons.arrow_back),
-    //             onPressed: () {
-    //               Navigator.of(context).pop();
-    //             },
-    //           ),
-    //           actions: <Widget>[
-    //             IconButton(
-    //               icon: Icon(Icons.dashboard),
-    //               onPressed: () {
-    //                 Navigator.of(context).pop();
-    //                 Navigator.of(context).pop();
-    //                 Navigator.of(context).pop();
-    //                 Navigator.of(context).pop();
-    //               },
-    //             ),
-    //           ],
-    //           title: Text(
-    //             profileName,
-    //             style: TextStyle(
-    //               fontSize: appBarTitle,
-    //               fontWeight: FontWeight.bold,
-    //             ),
-    //           ),
-    //         ),
-    //         // Body
-    //         body: SingleChildScrollView(
-    //           child: Column(
-    //             crossAxisAlignment: CrossAxisAlignment.center,
-    //             mainAxisAlignment: MainAxisAlignment.start,
-    //             children: [
-    //               // Profile Name
-    //               // Education
-    //               // Experience
-    //               // Extracurricular
-    //               // Honors
-    //               // Projects
-    //               // Qualifications
-    //               // References
-    //             ],
-    //           ),
-    //         ),
-    //         // Bottom Navigation Bar
-    //         bottomNavigationBar: BottomAppBar(
-    //             // color: Colors.transparent,
-    //             // // Row
-    //             // child: Row(
-    //             //   crossAxisAlignment: CrossAxisAlignment.center,
-    //             //   mainAxisAlignment: MainAxisAlignment.center,
-    //             //   children: <Widget>[
-    //             //     // Save Button
-    //             //     ElevatedButton(
-    //             //       onPressed: () {
-    //             //         OverWriteProfile(context, controllers);
-    //             //       },
-    //             //       child: Text(
-    //             //         'Save',
-    //             //         style: TextStyle(
-    //             //           color: Colors.black,
-    //             //           fontSize: 16.0,
-    //             //           fontWeight: FontWeight.bold,
-    //             //         ),
-    //             //       ),
-    //             //     ),
-    //             //     SizedBox(width: 20),
-    //             //     // Set As Primary Button
-    //             //     ElevatedButton(
-    //             //       onPressed: () => {},
-    //             //       child: Text(
-    //             //         'Set As Primary',
-    //             //         style: TextStyle(
-    //             //           color: Colors.black,
-    //             //           fontSize: 16.0,
-    //             //           fontWeight: FontWeight.bold,
-    //             //         ),
-    //             //       ),
-    //             //     ),
-    //             //     SizedBox(width: 20),
-    //             //     // Cancel Button
-    //             //     ElevatedButton(
-    //             //       onPressed: () {
-    //             //         Navigator.of(context).pop();
-    //             //       },
-    //             //       child: Text(
-    //             //         'Cancel',
-    //             //         style: TextStyle(
-    //             //           color: Colors.black,
-    //             //           fontSize: 16.0,
-    //             //           fontWeight: FontWeight.bold,
-    //             //         ),
-    //             //       ),
-    //             //     ),
-    //             //   ],
-    //             // ),
-    //             ),
-    //       );
-    //     }
-    //   },
-    // );
   }
 }
