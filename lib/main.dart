866c3f028c8d84c5ca15ebb72d27f67a0868e684
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-26 01:15:27 -0700

    Main File Update - Sanity Check For Jobs Directory Creation

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    CreateJobsDir();
     CreateProfileDir();
     return Consumer<ThemeProvider>(
       builder: (context, themeProvider, child) {
