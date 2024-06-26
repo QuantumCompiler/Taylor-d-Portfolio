c4d47323400662fc27da50d0ca9383cb654f4853
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-26 00:15:14 -0700

    Jobs Init Commit

---
+++ lib/src/Jobs/Jobs.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import '../Globals/Globals.dart';
+
+class JobsPage extends StatelessWidget {
+  const JobsPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            if (isDesktop()) {
+              Navigator.of(context).pop();
+              Navigator.of(context).pop();
+            } else if (isMobile()) {
+              Navigator.of(context).pop();
+            }
+          },
+        ),
+        title: Text(
+          jobsTitle,
+          style: TextStyle(
+            fontSize: appBarTitle,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * jobTileContainerWidth,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: standardSizedBoxHeight),
+              ListTile(
+                title: Text(
+                  jobsCreateNew,
+                ),
+                onTap: () {
+                  // Navigator.push(context, MaterialPageRoute(builder: (context) => NewProfilePage()));
+                },
+              ),
+              ListTile(
+                title: Text(
+                  jobsLoad,
+                ),
+                onTap: () {
+                  // Navigator.push(context, MaterialPageRoute(builder: (context) => LoadProfilePage()));
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
