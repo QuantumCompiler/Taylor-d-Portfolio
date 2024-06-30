d5c52f4206d32be330869153dbb0014ad0b54257
QuantumCompiler <tjlarrechea@gmail.com>
2024-06-30 16:19:41 -0700

    Settings Update - Settings Files Refactor

--- lib/src/Settings/Context/SettingsContext.dart
+++ lib/src/Settings/Context/SettingsContext.dart
@@ -1,22 +1,10 @@
 // Imports
 import 'package:flutter/material.dart';
+import '../Globals/SettingsGlobals.dart';
 import '../../Globals/Globals.dart';
 import '../../Jobs/Utilities/JobUtils.dart';
 import '../../Profile/Utilities/ProfileUtils.dart';
 import '../../Themes/Themes.dart';
-
-/* Parameters
-    settingsTileContainerWidth: Double for width of the settings tile container
-    settingsCurrentTheme: String for current theme setting
-    settingsDeleteAllProfiles: String for delete all profiles setting
-    settingsDeleteAllJobs: String for delete all jobs setting
-    settingsTitle: String for title of the settings page
-*/
-double settingsTileContainerWidth = 0.6;
-String settingsCurrentTheme = 'Switch Theme';
-String settingsDeleteAllProfiles = 'Delete All Profiles';
-String settingsDeleteAllJobs = 'Delete All Jobs';
-String settingsTitle = 'Settings';
 
 /*  appBar - App bar for the settings page
       Input:
---
+++ lib/src/Settings/Globals/SettingsGlobals.dart
@@ -0,0 +1,12 @@
+/* Parameters
+    settingsTileContainerWidth: Double for width of the settings tile container
+    settingsCurrentTheme: String for current theme setting
+    settingsDeleteAllProfiles: String for delete all profiles setting
+    settingsDeleteAllJobs: String for delete all jobs setting
+    settingsTitle: String for title of the settings page
+*/
+double settingsTileContainerWidth = 0.6;
+String settingsCurrentTheme = 'Switch Theme';
+String settingsDeleteAllProfiles = 'Delete All Profiles';
+String settingsDeleteAllJobs = 'Delete All Jobs';
+String settingsTitle = 'Settings';
