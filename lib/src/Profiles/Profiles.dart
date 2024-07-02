b514495c8f9e80e936d899fe6c28d35ad3cd0895
QuantumCompiler <tjlarrechea@gmail.com>
2024-07-02 15:06:24 -0700

    Profiles Files Refactor

--- lib/src/Profiles/Profiles.dart
+++ lib/src/Profiles/Profiles.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Context/ProfileContext.dart';
+import '../Context/ProfileContext.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
