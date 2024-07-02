b22873c45699cb3bc0c6c24aefdc9013315becd2
QuantumCompiler <tjlarrechea@gmail.com>
2024-07-02 15:05:37 -0700

    Context Files Refactor

---
+++ lib/src/Context/LoadProfilesContext.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+import '../Globals/ProfilesGlobals.dart';
+import '../Profiles/EditProfile.dart';
+import '../Utilities/ProfilesUtils.dart';
+import '../Globals/Globals.dart';
+
+/* appBar - AppBar for the load profile page
+      Input:
+        context: BuildContext of the page
+        profiles: List of profiles
+        state: Function to update the state of the page
+      Algorithm:
+          * Create a back button to return to the previous page
+          * Modify the navigation based on the device type
+          * Add a title to the AppBar
+      Output:
+        Returns an AppBar
+*/
+AppBar appBar(BuildContext context, final profiles, Function state) {
+  return AppBar(
+    leading: IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.of(context).pop();
+        state(() {});
+      },
+    ),
+    actions: <Widget>[
+      IconButton(
+        icon: Icon(Icons.dashboard),
+        onPressed: () {
+          if (isDesktop()) {
+            Navigator.of(context).pop();
+            Navigator.of(context).pop();
+            Navigator.of(context).pop();
+          } else if (isMobile()) {
+            Navigator.of(context).pop();
+            Navigator.of(context).pop();
+          }
+        },
+      ),
+    ],
+    title: Text(
+      profiles.isEmpty ? profilesEmptyTitle : loadProfilesTitle,
+      style: TextStyle(
+        fontSize: appBarTitle,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
+
+/*  loadingProfilesAppBar - AppBar for the loading profiles page
+      Input:
+        context: BuildContext of the page
+      Algorithm:
+          * Create a back button to return to the previous page
+          * Add a title to the AppBar
+      Output:
+        Returns an AppBar
+*/
+AppBar loadingProfilesAppBar(BuildContext context) {
+  return AppBar(
+    leading: IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () => Navigator.of(context).pop(),
+    ),
+    title: Text(
+      currentlyLoadingProfiles,
+    ),
+  );
+}
+
+/*  loadingProfilesContent - Body content for the loading profiles page
+      Input:
+        None
+      Algorithm:
+          * Display a CircularProgressIndicator
+      Output:
+        Returns a Center widget with a CircularProgressIndicator
+*/
+Center loadingProfilesContent() {
+  return const Center(child: CircularProgressIndicator());
+}
+
+/* loadProfileContent - Body content for the load profile page
+      Input:
+        context: BuildContext of the page
+        profiles: List of profiles
+        state: Function to update the state of the page
+      Algorithm:
+          * Create a container to hold the profile tiles
+          * Populate the container with a ListView of profile tiles
+      Output:
+        Returns a Center widget with a container of profile tiles
+*/
+Center loadProfileContent(BuildContext context, final profiles, Function state) {
+  return Center(
+    child: Container(
+      width: MediaQuery.of(context).size.width * profileTileContainerWidth,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: standardSizedBoxHeight),
+          Expanded(
+            child: ListView.builder(
+              itemCount: profiles.length,
+              itemBuilder: (context, index) {
+                return MouseRegion(
+                  cursor: SystemMouseCursors.click,
+                  child: ListTile(
+                    title: Text(profiles[index].path.split('/').last),
+                    trailing: IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text(
+                                "$deleteButton ${profiles[index].path.split('/').last}?",
+                                style: TextStyle(
+                                  fontSize: appBarTitle,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              content: Text(
+                                deleteProfilePrompt,
+                                style: TextStyle(
+                                  fontSize: secondaryTitles,
+                                ),
+                                textAlign: TextAlign.center,
+                              ),
+                              actions: <Widget>[
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    ElevatedButton(
+                                      child: Text(cancelButton),
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                    ),
+                                    SizedBox(width: standardSizedBoxWidth),
+                                    ElevatedButton(
+                                      child: Text(deleteButton),
+                                      onPressed: () async {
+                                        await DeleteProfile(profiles[index].path.split('/').last);
+                                        Navigator.of(context).pop();
+                                        state(() {});
+                                      },
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            );
+                          },
+                        );
+                      },
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EditProfilePage(profileName: profiles[index].path.split('/').last),
+                        ),
+                      ).then(
+                        (_) {
+                          state(() {});
+                        },
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    ),
+  );
+}
---
+++ lib/src/Context/SettingsContext.dart
@@ -0,0 +1,220 @@
+// Imports
+import 'package:flutter/material.dart';
+import '../Globals/SettingsGlobals.dart';
+import '../Globals/Globals.dart';
+import '../Utilities/JobUtils.dart';
+import '../Utilities/ProfilesUtils.dart';
+import '../Themes/Themes.dart';
+
+/*  appBar - App bar for the settings page
+      Input:
+        context: BuildContext of the application
+      Algorithm:
+          * Return an AppBar with a back button and title
+      Output:
+          Returns an AppBar with a back button and title
+*/
+AppBar appBar(BuildContext context) {
+  return AppBar(
+    leading: IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        if (isDesktop()) {
+          Navigator.of(context).pop();
+          Navigator.of(context).pop();
+        } else if (isMobile()) {
+          Navigator.of(context).pop();
+        }
+      },
+    ),
+    title: Text(
+      settingsTitle,
+      style: TextStyle(
+        fontSize: appBarTitle,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+  );
+}
+
+/*  bodyContent - Body content for the settings page
+      Input:
+        context: BuildContext
+        theme: ThemeProvider built from the ThemeProvider class
+      Algorithm:
+          * Return a center widget with a container for the settings
+          * Populate the container with a column of settings
+      Output:
+          Returns a Center widget with a container for the settings
+*/
+Center bodyContent(BuildContext context, ThemeProvider theme) {
+  return Center(
+    child: Container(
+      width: MediaQuery.of(context).size.width * settingsTileContainerWidth,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          switchTheme(theme),
+          delJobs(context),
+          delProfs(context),
+        ],
+      ),
+    ),
+  );
+}
+
+/*  delJobs - Delete all jobs
+      Input:
+        context: BuildContext
+      Algorithm:
+          * Show dialog to confirm deletion of all jobs
+          * Call DeleteAllJobs function if confirmed in the context of the platformDetect function
+      Output:
+          Returns a ListTile for deleting all jobs
+*/
+ListTile delJobs(BuildContext context) {
+  return ListTile(
+    title: Text(
+      settingsDeleteAllJobs,
+    ),
+    onTap: () {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+              'Delete All Jobs',
+              style: TextStyle(
+                fontSize: appBarTitle,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            content: Text(
+              'Would you like to delete all jobs? This cannot be undone.',
+              textAlign: TextAlign.center,
+            ),
+            actions: [
+              platformDetect(context, DeleteAllJobs),
+            ],
+          );
+        },
+      );
+    },
+  );
+}
+
+/*  delProfs - Delete all profiles
+      Input:
+        context: BuildContext
+      Algorithm:
+          * Show dialog to confirm deletion of all profiles
+          * Call DeleteAllProfiles function if confirmed in the context of the platformDetect function
+      Output:
+          Returns a ListTile for deleting all profiles
+*/
+ListTile delProfs(BuildContext context) {
+  return ListTile(
+    title: Text(
+      settingsDeleteAllProfiles,
+    ),
+    onTap: () {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(
+              'Delete All Profiles',
+              style: TextStyle(
+                fontSize: appBarTitle,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            content: Text(
+              'Would you like to delete all profiles? This cannot be undone.',
+              textAlign: TextAlign.center,
+            ),
+            actions: [
+              platformDetect(context, DeleteAllProfiles),
+            ],
+          );
+        },
+      );
+    },
+  );
+}
+
+/*  platformDetect - Detects the platform and returns the appropriate row
+      Input:
+        context: BuildContext
+        func: VoidCallback
+      Algorithm:
+          * If the platform is desktop, return an ElevatedButton for cancel and delete
+          * If the platform is mobile, return an IconButton for cancel and delete
+      Output:
+          Returns a Row with the appropriate buttons for the platform
+*/
+Row platformDetect(BuildContext context, VoidCallback func) {
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      if (isDesktop()) ...[
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: Text(
+            'Cancel',
+          ),
+        ),
+      ] else if (isMobile()) ...[
+        IconButton(
+          icon: Icon(Icons.cancel),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+      SizedBox(width: standardSizedBoxWidth),
+      if (isDesktop()) ...[
+        ElevatedButton(
+          onPressed: () {
+            func();
+            Navigator.of(context).pop();
+          },
+          child: Text(
+            'Delete',
+          ),
+        ),
+      ] else if (isMobile()) ...[
+        IconButton(
+          icon: Icon(Icons.save),
+          onPressed: () {
+            func();
+            Navigator.of(context).pop();
+          },
+        ),
+      ]
+    ],
+  );
+}
+
+/*  switchTheme - Switch for toggling between light and dark theme
+      Input:
+        theme: ThemeProvider built from the ThemeProvider class
+      Output:
+          Returns a SwitchListTile for toggling between light and dark theme
+*/
+SwitchListTile switchTheme(ThemeProvider theme) {
+  return SwitchListTile(
+    title: Text(
+      settingsCurrentTheme,
+    ),
+    value: theme.isDarkTheme,
+    onChanged: (value) {
+      theme.toggleTheme();
+    },
+  );
+}
