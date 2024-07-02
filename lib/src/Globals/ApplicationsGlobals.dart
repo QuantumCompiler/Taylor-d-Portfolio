c39d6a7f187ac4e3bedba3d1389ff14e30c4e264
QuantumCompiler <tjlarrechea@gmail.com>
2024-07-02 15:06:03 -0700

    Globals Files Recator

---
+++ lib/src/Globals/ApplicationsGlobals.dart
@@ -0,0 +1,8 @@
+// Sizes
+double applicationsTileContainerWidth = 0.6;
+double applicationsTitleSize = 24.0;
+double applicationsContainerWidth = 0.8;
+// Titles & Hints
+String applicationsTitle = 'Applications';
+String createNewApplicationTile = 'Create New Application';
+String newApplicationTitle = 'New Application';
