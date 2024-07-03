86201d852faceac66ce05b68545ce3a079e68758
QuantumCompiler <tjlarrechea@gmail.com>
2024-07-02 20:47:10 -0700

    Applications Globals Update - Created Methods To Process Application Content, Class For OpenAI Call

--- lib/src/Globals/ApplicationsGlobals.dart
+++ lib/src/Globals/ApplicationsGlobals.dart
@@ -6,3 +6,19 @@ double applicationsContainerWidth = 0.8;
 String applicationsTitle = 'Applications';
 String createNewApplicationTile = 'Create New Application';
 String newApplicationTitle = 'New Application';
+// API Key
+String apiKey = 'OPENAI_API_KEY';
+// OpenAI Models
+String gpt_3_5_turbo = 'gpt-3.5-turbo';
+String gpt_3_5_turbo_0125 = 'gpt-3.5-turbo-0125';
+String gpt_3_5_turbo_1106 = 'gpt-3.5-turbo-1106';
+String gpt_3_5_turbo_16k = 'gpt-3.5-turbo-16k';
+String gpt_4 = 'gpt-4';
+String gpt_4_0125_preview = 'gpt-4-0125-preview';
+String gpt_4_0613 = 'gpt-4-0613';
+String gpt_4_1106_preview = 'gpt-4-1106-preview';
+String gpt_4_turbo = 'gpt-4-turbo';
+String gpt_4_turbo_2024_04_09 = 'gpt-4-turbo-2024-04-09';
+String gpt_4_turbo_turbo_preview = 'gpt-4-turbo-preview';
+String gpt_4o = 'gpt-4o';
+String gpt_4o_2024_05_13 = 'gpt-4o-2024-05-13';
