4ace82f10b432650ad68f8c1bf3b167adb81e0d4
QuantumCompiler <tjlarrechea@gmail.com>
2024-07-09 21:55:02 -0700

    View Application Commit - Created Screen For Viewing A Previous Application

---
+++ lib/src/Applications/ViewApplication.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import '../Context/ViewApplicationContext.dart';
+import '../Utilities/ApplicationsUtils.dart';
+
+class ViewAppPage extends StatelessWidget {
+  final Application prevApp;
+  const ViewAppPage({
+    required this.prevApp,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+      future: prevApp.LoadAppData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Error'),
+              ),
+              body: Center(
+                child: Text('Error loading application data: ${snapshot.error}'),
+              ),
+            );
+          }
+          return Scaffold(
+            appBar: appBar(context, prevApp),
+            body: loadAppContent(context, prevApp),
+          );
+        } else {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Loading'),
+            ),
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
