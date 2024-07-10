bc4568886113c7f05507a5e5d198419b2a65d2b8
QuantumCompiler <tjlarrechea@gmail.com>
2024-07-10 16:30:41 -0700

    View Application Context Update - Created Bottom App Bar Context For Pages

--- lib/src/Context/Applications/ViewApplicationContext.dart
+++ lib/src/Context/Applications/ViewApplicationContext.dart
@@ -1,7 +1,48 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-// import '../Globals/ApplicationsGlobals.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+// import '../../Globals/ApplicationsGlobals.dart';
 import '../../Utilities/ApplicationsUtils.dart';
 import '../../Globals/Globals.dart';
+
+class PDFScreen extends StatelessWidget {
+  final Application prevApp;
+  const PDFScreen({super.key, required this.prevApp});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: FutureBuilder<String>(
+        future: prevApp.retrievePDFDir('Resume', 'main.pdf'),
+        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              final pdfFilePath = snapshot.data!;
+              final file = File(pdfFilePath);
+              if (file.existsSync()) {
+                return SfPdfViewer.file(file);
+              } else {
+                return Center(child: Text('File not found: $pdfFilePath'));
+              }
+            }
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
 
 AppBar appBar(BuildContext context, Application prevApp) {
   return AppBar(
@@ -59,6 +100,25 @@ SingleChildScrollView loadAppContent(BuildContext context, Application prevApp) 
           recCont(context, 'Scientific Skills Recommendations', prevApp.sciRecFile.readAsStringSync()),
         ],
       ),
+    ),
+  );
+}
+
+BottomAppBar bottomAppBar(BuildContext context, Application prevApp) {
+  return BottomAppBar(
+    child: Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          child: Text(
+            'View Portfolio',
+          ),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => PDFScreen(prevApp: prevApp)));
+          },
+        ),
+      ],
     ),
   );
 }
