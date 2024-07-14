b49730c94772e85f11fd70521aa864c898f05640
Taylor Larrechea <tjlarrechea@gmail.com>
2024-07-14 15:44:49 -0700

    Globals Update - Began Refactoring Globals And Removing Global Functions

--- lib/src/Globals/Globals.dart
+++ lib/src/Globals/Globals.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 //  Booleans
@@ -37,100 +36,6 @@ String jobsMasterDir = 'Jobs';
 String latexMasterDir = 'LaTeX';
 String profilesMasterDir = 'Profiles';
 
-// Application Directory
-Future<Directory> GetAppDir() async {
-  return await getApplicationDocumentsDirectory();
-}
-
-// Cache Directory
-Future<Directory> GetCacheDir() async {
-  return await getTemporaryDirectory();
-}
-
-// Applications Directory
-Future<Directory> GetApplicationsDir() async {
-  final appDir = await getApplicationDocumentsDirectory();
-  final applicationsDir = Directory('${appDir.path}/$applicationsMasterDir');
-  if (!applicationsDir.existsSync()) {
-    applicationsDir.create();
-  }
-  return applicationsDir;
-}
-
-// Jobs Directory
-Future<Directory> GetJobsDir() async {
-  final appDir = await getApplicationDocumentsDirectory();
-  final jobsDir = Directory('${appDir.path}/$jobsMasterDir');
-  if (!jobsDir.existsSync()) {
-    jobsDir.createSync();
-  }
-  return jobsDir;
-}
-
-// LaTeX Directory
-Future<Directory> GetLaTeXDir() async {
-  final appDir = await getApplicationDocumentsDirectory();
-  final latexDir = Directory('${appDir.path}/$latexMasterDir');
-  if (!latexDir.existsSync()) {
-    latexDir.createSync();
-  }
-  return latexDir;
-}
-
-// Profiles Directory
-Future<Directory> GetProfilesDir() async {
-  final appDir = await getApplicationDocumentsDirectory();
-  final profilesDir = Directory('${appDir.path}/$profilesMasterDir');
-  if (!profilesDir.existsSync()) {
-    profilesDir.createSync();
-  }
-  return profilesDir;
-}
-
-// Support Directory
-Future<Directory> GetSupportDir() async {
-  return await getApplicationSupportDirectory();
-}
-
-// Create Directory
-Future<void> CreateDir(Directory parentDir, String dirName) async {
-  final dir = Directory('${parentDir.path}/$dirName');
-  if (!dir.existsSync()) {
-    dir.createSync();
-  }
-}
-
-// Create Applications Directory
-Future<void> CreateApplicationsDir() async {
-  Directory appDir = await GetAppDir();
-  CreateDir(appDir, applicationsMasterDir);
-}
-
-// Create Jobs Directory
-Future<void> CreateJobsDir() async {
-  Directory appDir = await GetAppDir();
-  CreateDir(appDir, jobsMasterDir);
-}
-
-// Create LaTeX Directory
-Future<void> CreateLaTeXDir() async {
-  Directory appDir = await GetAppDir();
-  CreateDir(appDir, latexMasterDir);
-}
-
-// Create Profile Directory
-Future<void> CreateProfileDir() async {
-  Directory appDir = await GetAppDir();
-  CreateDir(appDir, profilesMasterDir);
-}
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-//  Files
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-Future<void> WriteFile(Directory dir, File file, String contents) async {
-  await file.writeAsString(contents);
-}
-
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 //  Size Box Parameters
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
@@ -142,3 +47,5 @@ double standardSizedBoxWidth = 20;
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 double appBarTitle = 24.0;
 double secondaryTitles = 16.0;
+
+double titleContainerWidth = 0.6;
