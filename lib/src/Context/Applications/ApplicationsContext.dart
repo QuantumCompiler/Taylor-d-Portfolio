d370bba267842fb1b2b9f687fd40b3daeb0bdb20
Taylor Larrechea <tjlarrechea@gmail.com>
2024-07-14 15:42:16 -0700

    Applications Context Update - Temporary Commenting Out

--- lib/src/Context/Applications/ApplicationsContext.dart
+++ lib/src/Context/Applications/ApplicationsContext.dart
@@ -4,53 +4,53 @@ import '../../Applications/NewApplication.dart';
 import '../../Globals/ApplicationsGlobals.dart';
 import '../../Globals/Globals.dart';
 
-AppBar appBar(BuildContext context) {
-  return AppBar(
-    leading: IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        if (isDesktop()) {
-          Navigator.of(context).pop();
-          Navigator.of(context).pop();
-        } else if (isMobile()) {
-          Navigator.of(context).pop();
-        }
-      },
-    ),
-    title: Text(
-      applicationsTitle,
-      style: TextStyle(
-        fontSize: appBarTitle,
-        fontWeight: FontWeight.bold,
-      ),
-    ),
-  );
-}
+// AppBar appBar(BuildContext context) {
+//   return AppBar(
+//     leading: IconButton(
+//       icon: Icon(Icons.arrow_back),
+//       onPressed: () {
+//         if (isDesktop()) {
+//           Navigator.of(context).pop();
+//           Navigator.of(context).pop();
+//         } else if (isMobile()) {
+//           Navigator.of(context).pop();
+//         }
+//       },
+//     ),
+//     title: Text(
+//       applicationsTitle,
+//       style: TextStyle(
+//         fontSize: appBarTitle,
+//         fontWeight: FontWeight.bold,
+//       ),
+//     ),
+//   );
+// }
 
-Center applicationsContent(BuildContext context) {
-  return Center(
-    child: Container(
-      width: MediaQuery.of(context).size.width * applicationsTileContainerWidth,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          SizedBox(height: standardSizedBoxHeight),
-          ListTile(
-            title: Text(
-              createNewApplicationTile,
-            ),
-            onTap: () => {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => NewApplicationPage())),
-            },
-          ),
-          ListTile(
-            title: Text(
-              'Load Applications',
-            ),
-            onTap: () => {Navigator.push(context, MaterialPageRoute(builder: (context) => LoadApplicationPage()))},
-          ),
-        ],
-      ),
-    ),
-  );
-}
+// Center applicationsContent(BuildContext context) {
+//   return Center(
+//     child: Container(
+//       width: MediaQuery.of(context).size.width * applicationsTileContainerWidth,
+//       child: Column(
+//         mainAxisAlignment: MainAxisAlignment.start,
+//         children: [
+//           SizedBox(height: standardSizedBoxHeight),
+//           ListTile(
+//             title: Text(
+//               createNewApplicationTile,
+//             ),
+//             onTap: () => {
+//               Navigator.push(context, MaterialPageRoute(builder: (context) => NewApplicationPage())),
+//             },
+//           ),
+//           ListTile(
+//             title: Text(
+//               'Load Applications',
+//             ),
+//             onTap: () => {Navigator.push(context, MaterialPageRoute(builder: (context) => LoadApplicationPage()))},
+//           ),
+//         ],
+//       ),
+//     ),
+//   );
+// }
