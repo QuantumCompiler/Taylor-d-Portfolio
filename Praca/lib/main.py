a49024a658d726121012ea9c27998a218f4055fe
Taylor Larrechea <tjlarrechea@gmail.com>
2024-10-07 22:07:45 -0700

    Praca Updates

--- Praca/lib/main.py
+++ Praca/lib/main.py
@@ -1,23 +1,35 @@
-# from transformers import AutoModelForCausalLM, AutoTokenizer
-# import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
 
-# # Path to the model directory
-# model_dir = "../Model"
+def PracaPrompt(model, tokenizer, device):
+    # Get input
+    userInput = input("Enter your prompt for Praca: ")
+    # Tokenize the input and move to GPU
+    input_ids = tokenizer(userInput, return_tensors="pt").to(device)
+    # Generate the output using sampling-based techniques
+    outputs = model.generate(
+        **input_ids,
+        do_sample=True,           # Enable sampling to introduce randomness
+        top_k=100,                # Consider the top 100 options for sampling
+        top_p=0.95,               # Or consider cumulative probability up to 95%
+        temperature=0.75,         # Control randomness; 1.0 is neutral, lower is more deterministic
+        max_length=1000,          # Adjust the max length to ensure long enough outputs
+        num_return_sequences=1    # Number of different sequences to generate
+    )
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-# # Load the tokenizer
-# tokenizer = AutoTokenizer.from_pretrained(model_dir)
-
-# # Load the model using the safetensors format
-# model = AutoModelForCausalLM.from_pretrained(model_dir, torch_dtype=torch.float16, low_cpu_mem_usage=True)
-
-# # Test the setup with a specific prompt
-# input_text = "Give me a funny dad joke:"
-
-# # Tokenize the input text
-# inputs = tokenizer(input_text, return_tensors="pt")
-
-# # Generate the output using the model
-# outputs = model.generate(**inputs, max_length=500, num_return_sequences=1, no_repeat_ngram_size=2)
-
-# # Decode and print the generated text
-# print("Generated Text: ", tokenizer.decode(outputs[0], skip_special_tokens=True))
+if __name__ == "__main__":
+    # Load the model using the pre-trained weights
+    tokenizer = AutoTokenizer.from_pretrained("../Model")
+    model = AutoModelForCausalLM.from_pretrained("../Model")
+    # Check if a GPU is available and move the model to GPU
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+    else:
+        device = torch.device("cpu")
+    model.to(device)
+    # Generate the output
+    ret = PracaPrompt(model, tokenizer, device)
+    print(ret)
